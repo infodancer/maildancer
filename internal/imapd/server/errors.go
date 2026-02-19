775c9e452e1d303d9678a3a04b5a164869f7e1a4
Matthew Hunter <matthewjayhunter@gmail.com>
2026-02-19 01:29:18 -0600

    Remove hand-rolled IMAP protocol and server packages
    Replaced by emersion/go-imap/v2 which handles all protocol parsing,
    state machine, TLS, and connection management.
    
    https://github.com/infodancer/imapd/issues/5

--- internal/imapd/server/errors.go
+++
@@ -1,8 +0,0 @@
-package server
-
-import "errors"
-
-var (
-	// ErrAlreadyTLS is returned when attempting to upgrade an already-TLS connection.
-	ErrAlreadyTLS = errors.New("connection already using TLS")
-)
